ebc376c8511e076008999f5d2302c41a6b7ee958
Maksym-Shcherbak <130525995+Maksym-Shcherbak@users.noreply.github.com>
2023-08-26 13:35:44 +0000

    Deploying to gh-pages from @ PavloKuchma/jspractice@c3114fbd2a31d13c89eebd8cb909812999ba6cd8 🚀

---
+++ index.1343cea6.js
@@ -0,0 +1,2 @@
+const t=document.querySelector(".todo");document.querySelector(".todo__items");let e={};t.addEventListener("input",(function(t){const{name:o,value:n}=t.target;e[o]=n,console.log(e),localStorage.setItem("todo",JSON.stringify(e))}));
+//# sourceMappingURL=index.1343cea6.js.map
---
+++ index.f553871e.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelector(".todo"),t=(document.querySelector(".todo__items"),{});e.addEventListener("input",(function(e){var o=e.target,n=o.name,r=o.value;t[n]=r,console.log(t),localStorage.setItem("todo",JSON.stringify(t))}))}();
+//# sourceMappingURL=index.f553871e.js.map
