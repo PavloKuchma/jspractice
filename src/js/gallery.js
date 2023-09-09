9ccdc5aae4a0089e0f4f1c2d4b071eca179240cd
PavloKuchma <111251902+PavloKuchma@users.noreply.github.com>
2023-09-09 16:17:25 +0300

    task-4

--- src/js/gallery.js
+++ src/js/gallery.js
@@ -1,4 +1,32 @@
 import { UnsplashAPI } from './UnsplashAPI.js';
+import Pagination from 'tui-pagination';
+import 'tui-pagination/dist/tui-pagination.min.css';
+import { createGalleryCard } from './galleryMarkup.js';
+
+const container = document.getElementById('tui-pagination-container');
+const galleryList = document.querySelector('.js-gallery');
+const options = {
+  totalItems: 0,
+  itemsPerPage: 12,
+  visiblePages: 5,
+  page: 1,
+};
+
+const pagination = new Pagination(container, options);
+
+const page = pagination.getCurrentPage();
 
 const api = new UnsplashAPI();
-api.getPopularPhotos(1).then(res => console.log(res));
+api.getPopularPhotos(page).then(({ total, results }) => {
+  pagination.reset(total);
+  const markup = createGalleryCard(results);
+  galleryList.innerHTML = markup;
+});
+
+pagination.on('afterMove', event => {
+  const currentPage = event.page;
+  api.getPopularPhotos(currentPage).then(({ total, results }) => {
+    const markup = createGalleryCard(results);
+    galleryList.innerHTML = markup;
+  });
+});
