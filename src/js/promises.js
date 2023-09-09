44e1ce34da80540965825a6c3820a227ef8a6695
Maksym-Shcherbak <max.scherbak93@gmail.com>
2023-09-09 15:51:35 +0300

    task 7

--- src/js/promises.js
+++ src/js/promises.js
@@ -27,7 +27,6 @@
 //     console.log(error)
 // })
 
-
 // console.log("hello world");
 
 // Task 4
@@ -55,3 +54,24 @@
 //     setTimeout(() => resolve("Hello world!"), 2000)
 // })
 //     .then(data => console.log(data));
+
+//TASK 6
+// - Використовуй prompt та повертай значення звідти.
+// - Створи функцію, яка буде набувати значення з prompt і всередині якої буде проміс.
+// Якщо значення не є числом, відхиляй проміс та логіруй "error".
+// Якщо значення парне, вирішуй проміс та повертай "even" через 1 секунду.
+// Якщо значення не парне, вирішуй проміс та повертай "odd" через 2 секунди.
+
+// const message = Number(prompt('Enter something'));
+
+// function checkNumber(message) {
+//   return new Promise((res, rej) => {
+//     console.log(message);
+//     if (Number.isNaN(message)) rej('ERROR');
+//     if (message % 2 === 0) setTimeout(() => res('even'), 1000);
+//     if (message % 2 !== 0) setTimeout(() => res('odd'), 2000);
+//   });
+// }
+// checkNumber(message)
+//   .then(response => console.log(response))
+//   .catch(error => console.log(error));
